940fa2e01bbd46d87e24f3bde29f308f98be22d5
aramecki <altair842.yt@gmail.com>
2024-10-04 12:14:34 +0200

    Cambiato font generale app, valutare se tenere. Continuare grafica cards.

--- app/src/main/java/com/android/mycargenie/ui/theme/Type.kt
+++ app/src/main/java/com/android/mycargenie/ui/theme/Type.kt
@@ -2,14 +2,22 @@ package com.android.mycargenie.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.android.mycargenie.R
+
+val WorkSans = FontFamily(
+    Font(R.font.worksans_regular),
+    Font(R.font.worksans_bold, FontWeight.Bold),
+    Font(R.font.worksans_semibold, FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = WorkSans,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
