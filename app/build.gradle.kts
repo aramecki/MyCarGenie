d6c3667362f14f713a313d87b6ebfa716bcbfdc7
aramecki <altair842.yt@gmail.com>
2024-12-05 10:13:04 +0100

    CHANGELOG: - Risolto bug per cui una volta eseguite diverse operazioni il tasto indietro costringeva a ripercorrere l'intera strada prima di chiudere l'app.
    TODO:
    -Aggiungere icone informative dei parametri in ProfileSettings.
    -Generalizzare funzione del d
    atapicker come in ExpSettings.
    
    MAJOR:
    -Aggiungere più profili configurabili.
    -Aggiungere controllo delle spese dettagliato.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.app.mycargenie"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
